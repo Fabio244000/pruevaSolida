cf0cf495741813a4566ff17ee3522996054f3884
Fabio <fabio.nm.19@gmail.com>
2022-02-16 00:21:07 -0500

    Se agrego una nueva bse de datos

--- tiendadjango/settings.py
+++ tiendadjango/settings.py
@@ -80,12 +80,8 @@ WSGI_APPLICATION = 'tiendadjango.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'mysql_cymysql',
-        'NAME': 'aes_tiendadjango',
-        'USER': 'fabionm',
-        'PASSWORD': '12345',
-        'HOST': 'localhost',
-        'PORT': '3306'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
